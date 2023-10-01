07a5ce3da29cee7dc9738be6459f8fffe9a725f0
Pierre DESMET <pierre.desmet@ensae.net>
2023-10-01 21:00:09 +0200

    Correction d'une erreur dans le calcul des mensualités

--- app.py
+++ app.py
@@ -1,43 +1,19 @@
+"""Simulateur du montant que pourra coûter notre futur logis à Lisa et moi"""
 import datetime
-import numpy as np
 import streamlit as st
 from PIL import Image
 
-
-def sep_milliers(nb, nb_dec=2):
-    """
-    Usage :
-    >>> sep_milliers(1254839.1245) --> '1 254 839.12'
-    nb peut etre une string ou un nombre.
-    """
-    if nb != nb:
-        return nb
-    if not isinstance(nb, str):
-        nb = str(nb)
-    if (nb_dec == 0) and ('.' in nb):
-        nb = nb[: nb.index('.')]
-    decimales = '' if '.' not in nb else nb[nb.index('.'):]
-    nb = nb if '.' not in nb else nb[: nb.index('.')]
-    d, r = divmod(len(nb), 3)
-    res = ' '.join(([nb[:r]] + [nb[i:i + 3] for i in np.arange(r, len(nb), 3)]))
-    return res.strip() + decimales[:nb_dec + 1]
-
-
-LIEU = 'SAINT-GERMAIN EN LAYE'
-APPART_OU_MAISON = 'appartement'
-NEUF_OU_ANCIEN = 'ancien'
-DATE_ACHAT_FUTUR_LOGEMENT = datetime.date(2028, 1, 1)
+from fonctions import (
+    lieu_to_inflation_appart,
+    lieu_to_inflation_maison,
+    get_mt_mensualités,
+    sep_milliers,
+    nb_mois_depuis_que_lisa_économise,
+    montant_qui_sera_remboursé_à_date
+)
 
 # Hypothèses
-GAIN_MOYEN_PAR_MOIS_PDE = 1500
-GAIN_MOYEN_PAR_MOIS_LVO = 2000
-APPORT_PDE_ACTUEL = 50_000
 SECURITE_LISA = 15_000
-
-AVEC_VENTE_APPARTEMENT_CACHAN = True
-SALAIRE_NET_MENSUEL_LVO_À_DATE_ACHAT = 3200
-SALAIRE_NET_MENSUEL_PDE_À_DATE_ACHAT = 3600
-CREDIT_A_LA_BNP = True
 TAUX_CRÉDITS_PUBLIC = 0.0392
 # https://www.human-immobilier.fr/content/pdf/bareme_honoraires_human_immobilier.pdf
 TAUX_FRAIS_AGENCE = 0.045
@@ -57,88 +33,59 @@ TAUX_MAX_ENDETTEMENT = 0.35  # assurance comprise
 DURÉE_MAX_CRÉDIT_EN_MOIS = 25 * 12
 
 
-# en 5 ans (https://www.meilleursagents.com/prix-immobilier/cachan-94230/rue-de-reims-2017464/1/)
-lieu_to_inflation_appart = {
-    'RUEIL-MALMAISON': 0.112,
-    'HOUILLES': 0.158,
-    'VÉSINET': 0.121,
-    'MAISONS-LAFFITTE': 0.121,
-    'CACHAN': 0.15,
-    'SAINT-GERMAIN EN LAYE': 0.206
-}
-
-# en 5 ans (https://www.meilleursagents.com/prix-immobilier/cachan-94230/rue-de-reims-2017464/1/)
-lieu_to_inflation_maison = {
-    'RUEIL-MALMAISON': 0.096,
-    'HOUILLES': 0.150,
-    'VÉSINET': 0.188,
-    'MAISONS-LAFFITTE': 0.145,
-    'CACHAN': 0.223,
-    'SAINT-GERMAIN EN LAYE': 0.114
-}
-
-
-def nb_mois_depuis_que_lisa_économise():
-    dt_début_INSPART = datetime.date(2022, 11, 1)
-    return (datetime.date.today() - dt_début_INSPART).days // 30.5
-
-
-def montant_qui_sera_remboursé_à_date(date=datetime.date.today()):
-    """Le montant qui aura été remboursé à la `date`"""
-    nb_mois_depuis_début_du_prêt = round((date - DATE_DÉBUT_DU_PRÊT_EXISTANT).days / 30.5)
-    montant_qui_sera_remboursé = nb_mois_depuis_début_du_prêt * MONTANT_REMBOURSÉ_PAR_MOIS
-    return montant_qui_sera_remboursé
-
-
-APPORT_LVO_ACTUEL = int(
-    GAIN_MOYEN_PAR_MOIS_LVO * nb_mois_depuis_que_lisa_économise() - SECURITE_LISA
-)
 st.set_page_config(
     page_title='Estimation logement',
     page_icon=Image.open("logo.png")
 )
 
 st.title('Estimation logement 2028')
-
-
 st.sidebar.markdown("## Modalités")
 select_ville = st.sidebar.selectbox('Ville', sorted(lieu_to_inflation_maison.keys()))
 select_appart_ou_maison = st.sidebar.selectbox(
     'Appartement ou maison', ['Appartement', 'Maison']
 )
 select_neuf_ancien = st.sidebar.selectbox('Neuf ou ancien', ['Ancien', 'Neuf'])
-select_date_achat = st.sidebar.date_input("Date achat futur logement", DATE_ACHAT_FUTUR_LOGEMENT)
+select_date_achat = st.sidebar.date_input('Date achat futur logement', datetime.date(2028, 1, 1))
 
 st.sidebar.markdown("## Hypothèses")
 select_avec_vente_appartement = st.sidebar.checkbox(
-    "Avec vente appartement de Cachan", AVEC_VENTE_APPARTEMENT_CACHAN
+    "Avec vente appartement de Cachan", True
 )
-select_avec_crédit_BNP = st.sidebar.checkbox("Avec taux du crédit BNP 2%", CREDIT_A_LA_BNP)
-select_tx_frais_agence = st.sidebar.slider("Frais **d'agence**", 0.02, 0.06, TAUX_FRAIS_AGENCE)
+select_avec_crédit_BNP = st.sidebar.checkbox("Avec taux avantageux BNP", True)
+select_tx_nominal = st.sidebar.slider("Taux nominal public", 0.01, 0.05, TAUX_CRÉDITS_PUBLIC)
+select_tx_frais_agence = st.sidebar.slider("[Frais d'agence](https://www.human-immobilier.fr/content/pdf/bareme_honoraires_human_immobilier.pdf)", 0.02, 0.06, TAUX_FRAIS_AGENCE)
+select_nb_années_pr_rembourser = st.sidebar.slider(
+    "Nombre d'années pour rembourser",
+    min_value=15, max_value=25, value=25, step=5
+)
 select_gain_mensuel_pde = st.sidebar.slider(
     'Gain mensuel Pierre',
-    min_value=1000, max_value=2500, value=GAIN_MOYEN_PAR_MOIS_PDE, step=100
+    min_value=1000, max_value=2500, value=1500, step=100
 )
+
 select_gain_mensuel_lvo = st.sidebar.slider(
     'Gain mensuel Lisa',
-    min_value=1000, max_value=2500, value=GAIN_MOYEN_PAR_MOIS_LVO, step=100
+    min_value=1000, max_value=2500, value=2000, step=100
 )
 select_apport_pde = st.sidebar.slider(
-    'Apport Pierre actuel',
-    min_value=20_000, max_value=150_000, value=APPORT_PDE_ACTUEL, step=5000
+    'Apport actuel Pierre',
+    min_value=20_000, max_value=150_000, value=50_000, step=5000
+)
+apport_lvo_actuel_default = int(
+    select_gain_mensuel_lvo * nb_mois_depuis_que_lisa_économise() - SECURITE_LISA
 )
 select_apport_lvo = st.sidebar.slider(
-    'Apport Lisa actuel',
-    min_value=20_000, max_value=150_000, value=APPORT_LVO_ACTUEL, step=5000
+    'Apport actuel Lisa',
+    min_value=20_000, max_value=150_000, value=apport_lvo_actuel_default, step=5000
 )
 select_w_mensuel_pde_date_achat = st.sidebar.slider(
     "Salaire mensuel net av. impôt Pierre à date d'achat",
-    min_value=3000, max_value=4500, value=SALAIRE_NET_MENSUEL_PDE_À_DATE_ACHAT, step=100
-)
+    min_value=3000, max_value=5000, value=4200, step=100
+)  # En sept. 2023, 3624 * 13 / 12 = 3930 de mensuel net avant impôt
 select_w_mensuel_lvo_date_achat = st.sidebar.slider(
     "Salaire mensuel net av. impôt Lisa à date d'achat",
-    min_value=3000, max_value=4500, value=SALAIRE_NET_MENSUEL_LVO_À_DATE_ACHAT
-)
+    min_value=3000, max_value=5000, value=3300
+)  # En 2023, 36 174 € / 12 = 3015€ de mensuel net avant impôt
 
 
 # Les dépendances
@@ -146,6 +93,8 @@ nb_mois_restants_avant_achat = round(
     (select_date_achat - datetime.date.today()).days / 30.5
 )
 montant_qui_sera_remboursé = montant_qui_sera_remboursé_à_date(
+    date_début_du_prêt_existant=DATE_DÉBUT_DU_PRÊT_EXISTANT,
+    mt_remboursé_par_mois=MONTANT_REMBOURSÉ_PAR_MOIS,
     date=select_date_achat
 )
 prix_estimé_revente = PRIX_APPARTEMENT_CACHAN * (1 + lieu_to_inflation_appart['CACHAN'])
@@ -160,19 +109,25 @@ apport_qui_sera_apporté_pde -= dû_à_la_banque * select_avec_vente_appartement
 apport_qui_sera_apporté_lvo = select_apport_lvo
 apport_qui_sera_apporté_lvo += select_gain_mensuel_lvo * nb_mois_restants_avant_achat
 
-capacité_max_emprunt_pde = TAUX_MAX_ENDETTEMENT * (
+mensualité_max_pde = TAUX_MAX_ENDETTEMENT * (
     select_w_mensuel_pde_date_achat - (
         (not select_avec_vente_appartement) * (MONTANT_REMBOURSÉ_PAR_MOIS + CHARGES_MENSUELLES)
     )
 )
-capacité_max_emprunt_pde *= DURÉE_MAX_CRÉDIT_EN_MOIS
-capacité_max_emprunt_lvo = TAUX_MAX_ENDETTEMENT * select_w_mensuel_lvo_date_achat
-capacité_max_emprunt_lvo *= DURÉE_MAX_CRÉDIT_EN_MOIS
+capacité_max_emprunt_pde = mensualité_max_pde * DURÉE_MAX_CRÉDIT_EN_MOIS
+mensualité_max_lvo = TAUX_MAX_ENDETTEMENT * select_w_mensuel_lvo_date_achat
+capacité_max_emprunt_lvo = mensualité_max_lvo * DURÉE_MAX_CRÉDIT_EN_MOIS
 
 capacité_emprunt_totale = capacité_max_emprunt_pde + capacité_max_emprunt_lvo
 montant_total_qui_sera_apporté = apport_qui_sera_apporté_pde + apport_qui_sera_apporté_lvo
 
+mensualité = get_mt_mensualités(
+    mt_emprunt=capacité_emprunt_totale,
+    tx_nominal=select_tx_nominal / (1 + select_avec_crédit_BNP),
+    nb_mois=select_nb_années_pr_rembourser * 12
+)
 prix_maximal_appartement = montant_total_qui_sera_apporté + capacité_emprunt_totale
+coût_crédit = mensualité * 12 * select_nb_années_pr_rembourser - capacité_emprunt_totale
 
 st.markdown('Prix maximal du logement : ' + sep_milliers(prix_maximal_appartement, 0) + ' €')
 st.markdown("Attention, il faut prendre en compte :")
@@ -190,9 +145,7 @@ prix_maximal_appartement = round(
 st.markdown(f"* L'inflation : {sep_milliers(prix_maximal_appartement, 0)} €")
 
 
-prix_maximal_appartement -= (
-    (TAUX_CRÉDITS_PUBLIC / (1 + select_avec_crédit_BNP)) * capacité_emprunt_totale
-)
+prix_maximal_appartement -= coût_crédit
 st.markdown(f"* Le coût du crédit (hors assurance) : {sep_milliers(prix_maximal_appartement, 0)} €")
 
 frais_de_notaire = 0.08 if select_neuf_ancien == 'Ancien' else 0.03
@@ -209,3 +162,19 @@ st.markdown('-' * 3)
 st.markdown("Pour obtenir cet appartement il nous faut :")
 st.markdown(f'* avoir un apport de {sep_milliers(montant_total_qui_sera_apporté, 0)} € ')
 st.markdown(f'* obtenir un emprunt de {sep_milliers(capacité_emprunt_totale, 0)} €')
+
+st.markdown(f'Les mensualités : {sep_milliers(mensualité)} €')
+
+
+st.markdown(f'Le coût du crédit : {sep_milliers(coût_crédit, 0)} €')
+
+
+st.markdown("TODO: prendre en compte l'assurance du prêt, et les autres composantes du TAEG...")
+# https://www.service-public.fr/particuliers/vosdroits/F2456
+# Intérêts bancaires calculés sur la base du taux actuariel : Taux qui permet de calculer le montant effectif des intérêts que l'emprunteur doit verser au prêteur
+# Frais de dossier (payés à la banque)
+# Frais payés ou dus à des intermédiaires intervenus dans l'octroi du prêt (courtier par exemple)
+# Coût de l'assurance emprunteur
+# Frais de garanties (hypothèque ou cautionnement)
+# Frais d'évaluation du bien immobilier (payés à un agent immobilier)
+# Tous les autres frais qui vous sont imposés pour l'obtention du crédit (frais de tenue de compte, en cas d'obligation d'ouverture de compte dans la banque qui octroie le prêt)
---
+++ fonctions.py
@@ -0,0 +1,77 @@
+"""Ce script contient les fonctions utilisées par l'application"""
+import datetime
+import numpy as np
+
+
+# en 5 ans (https://www.meilleursagents.com/prix-immobilier/cachan-94230/rue-de-reims-2017464/1/)
+lieu_to_inflation_appart = {
+    'RUEIL-MALMAISON': 0.112,
+    'HOUILLES': 0.158,
+    'VÉSINET': 0.121,
+    'MAISONS-LAFFITTE': 0.121,
+    'CACHAN': 0.15,
+    'SAINT-GERMAIN EN LAYE': 0.206
+}
+
+# en 5 ans (https://www.meilleursagents.com/prix-immobilier/cachan-94230/rue-de-reims-2017464/1/)
+lieu_to_inflation_maison = {
+    'RUEIL-MALMAISON': 0.096,
+    'HOUILLES': 0.150,
+    'VÉSINET': 0.188,
+    'MAISONS-LAFFITTE': 0.145,
+    'CACHAN': 0.223,
+    'SAINT-GERMAIN EN LAYE': 0.114
+}
+
+
+def get_mt_mensualités(mt_emprunt: float, tx_nominal: float, nb_mois: int):
+    """
+    À partir du montant à emprunter, du taux nominal du crédit immobilier et
+    du nombre d'échéances, retourne le montant de chaque mensualité dans le cas
+    d'un prêt ammortissable à taux fixe.
+    Source :
+    https://immobilier.lefigaro.fr/financer/guide-financement-immobilier/
+    1288-pret-amortissable-definition-et-calcul-de-la-mensualite/
+    """
+    return ((mt_emprunt * tx_nominal) / 12) / (1 - (1 + (tx_nominal / 12))**(-nb_mois))
+
+
+# Un test, conformément à cette page :
+# https://www.meilleurtaux.com/credit-immobilier/simulation-de-pret-immobilier/
+# calcul-des-mensualites.html
+assert round(get_mt_mensualités(230_000, 0.02, 20 * 12)) == 1164
+
+
+def sep_milliers(nb, nb_dec=2):
+    """
+    Usage :
+    >>> sep_milliers(1254839.1245) --> '1 254 839.12'
+    nb peut etre une string ou un nombre.
+    """
+    if nb != nb:
+        return nb
+    if not isinstance(nb, str):
+        nb = str(nb)
+    if (nb_dec == 0) and ('.' in nb):
+        nb = nb[: nb.index('.')]
+    decimales = '' if '.' not in nb else nb[nb.index('.'):]
+    nb = nb if '.' not in nb else nb[: nb.index('.')]
+    d, r = divmod(len(nb), 3)
+    res = ' '.join(([nb[:r]] + [nb[i:i + 3] for i in np.arange(r, len(nb), 3)]))
+    return res.strip() + decimales[:nb_dec + 1]
+
+
+def nb_mois_depuis_que_lisa_économise():
+    dt_début_INSPART = datetime.date(2022, 11, 1)
+    return (datetime.date.today() - dt_début_INSPART).days // 30.5
+
+
+def montant_qui_sera_remboursé_à_date(
+    date_début_du_prêt_existant,
+    mt_remboursé_par_mois,
+    date=datetime.date.today()
+):
+    """Le montant qui aura été remboursé à la `date`"""
+    nb_mois_depuis_début_du_prêt = round((date - date_début_du_prêt_existant).days / 30.5)
+    montant_qui_sera_remboursé = nb_mois_depuis_début_du_prêt * mt_remboursé_par_mois
+    return montant_qui_sera_remboursé
